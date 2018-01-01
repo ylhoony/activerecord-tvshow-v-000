40fff15b6b02407cbfd43270dbea66bd9d63c2c4
Hoon Lee <yl.hoony@gmail.com>
2018-01-01 09:44:12 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,8 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     Show.order(rating: :desc).limit(1)[0]
   end
+
+  def self.least_popular_show
+    Show.order(rating: :asc).limit(1)[0]
+  end
 end
